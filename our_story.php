d7208007c23239bd796561b5a593af72d4c3b6fa
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2023-02-10 14:36:02 +0000

    hero

--- our_story.php
+++ our_story.php
@@ -14,7 +14,6 @@ $story_result = $story->fetch_assoc();
         <div class="hero-container">
             <div class="hero-title text-center ">
                 <h1>Our Story</h1>
-                <p><?= $wedding_name; ?></p>
 
             </div>
             <div class="hero-img-card">
