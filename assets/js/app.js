a604a95d6640cf8439c96ceb3e2df97ee56b0048
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2023-02-11 09:09:11 +0000

    error

--- assets/js/app.js
+++ assets/js/app.js
@@ -102,11 +102,11 @@ function navslide(){
 //     }
 // }
 
-// // When the user clicks on the button, scroll to the top of the document
-// function scrolltotop() {
-//     document.body.scrollTop = 0; // For Safari
-//     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-//   }
+// When the user clicks on the button, scroll to the top of the document
+function scrolltotop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
 
 // cookie policy
 $(window).on('load', function() {
